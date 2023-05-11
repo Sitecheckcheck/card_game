6aff66c01837af8bbee42b3aa4d77fd9901e1828
Pavel <pvdanilov88@inbox.ru>
2023-05-11 23:37:08 +0300

    реализация выбора сложности игры

---
+++ script.js
@@ -0,0 +1,54 @@
+const appEl = document.getElementById("app");
+
+renderApp();
+
+function renderApp() {
+  const appHtml = `
+    <form class="body">
+        <div class="start">
+            <h2 class="start__title">Выбери <br> сложность</h2>
+            <div class="levels">
+                <div class="form_radio_btn">
+                    <input class="level-input" id="level-1" type="radio" name="level" value="1">
+                    <label class="level-label" for="level-1">1</label>
+                </div>
+                 
+                <div class="form_radio_btn">
+                    <input class="level-input" id="level-2" type="radio" name="level" value="2">
+                    <label class="level-label" for="level-2">2</label>
+                </div>
+                 
+                <div class="form_radio_btn">
+                    <input class="level-input" id="level-3" type="radio" name="level" value="3">
+                    <label class="level-label" for="level-3">3</label>
+                </div>
+            </div>
+            <button id="buttonStart" class="button-start">Старт</button>
+        </div>
+    </form>
+    `;
+
+  appEl.innerHTML = appHtml;
+
+  document.getElementById("buttonStart").addEventListener("click", () => {
+    let levelElements = document.querySelectorAll(".level-input");
+
+    for (const levelElement of levelElements) {
+      if (levelElement.checked) {
+        level = levelElement.value;
+        break;
+      }
+    }
+
+    renderGeme(level);
+  });
+}
+
+function renderGeme(level) {
+  const appHtml = `
+    <h1>
+        Уровень ${level}
+    </h1>
+    `;
+  appEl.innerHTML = appHtml;
+}
